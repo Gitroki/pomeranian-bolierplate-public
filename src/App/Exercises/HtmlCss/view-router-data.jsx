67dfa53210db60e74cdce274b47ae734cff3708f
Roksana <roksana.koronkiewicz@gmail.com>
2023-08-29 20:57:04 +0200

    classname

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,13 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { htmlCssRouterMetaData } from './SelectorsAndCascade/router-data';
+import { textFundamentsRouterMetaData } from './TextFundaments/router-data';
+import { standardTagsRouterMetaData } from './StandardTags/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   htmlCssRouterMetaData,
+  textFundamentsRouterMetaData,
+  standardTagsRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
