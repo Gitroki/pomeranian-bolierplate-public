3d234e65c54b1e2296c488acef1d9993e73dc10c
Roksana <roksana.koronkiewicz@gmail.com>
2023-08-30 18:52:47 +0200

    opacity

--- src/App/Exercises/HtmlCss/Colors/Colors.jsx
+++ src/App/Exercises/HtmlCss/Colors/Colors.jsx
@@ -2,13 +2,22 @@ import './styles.css';
 
 export const Colors = () => {
   return (
-    <div>
-      <h2 className="colorName">Jestem pomalowany przy uzyciu "colorName"</h2>
-      <h2 className="shortenHex">Jestem pomalowany przy uzyciu "shortenHex"</h2>
-      <h2 className="hex">Jestem pomalowany przy uzyciu "hex"</h2>
-      <h2 className="rgb">Jestem pomalowany przy uzyciu "rgb"</h2>
-      <h2 className="rgba">Jestem pomalowany przy uzyciu "rgba"</h2>
-      <h2 className="hsl">Jestem pomalowany przy uzyciu "rgba"</h2>
-    </div>
+    <>
+      <div>
+        <h2 className="colorName">Jestem pomalowany przy uzyciu "colorName"</h2>
+        <h2 className="shortenHex">
+          Jestem pomalowany przy uzyciu "shortenHex"
+        </h2>
+        <h2 className="hex">Jestem pomalowany przy uzyciu "hex"</h2>
+        <h2 className="rgb">Jestem pomalowany przy uzyciu "rgb"</h2>
+        <h2 className="rgba">Jestem pomalowany przy uzyciu "rgba"</h2>
+        <h2 className="hsl">Jestem pomalowany przy uzyciu "rgba"</h2>
+      </div>
+      <hr />
+      <div>
+        <div className="opacity">Jetsem</div>
+        <div className="rgbaopacity">Jetsem</div>
+      </div>
+    </>
   );
 };
