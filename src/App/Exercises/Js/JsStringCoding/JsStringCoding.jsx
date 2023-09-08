eed13b2fc80ce34a0e9394e3156c1ee851664c22
Roksana <roksana.koronkiewicz@gmail.com>
2023-09-08 20:52:02 +0200

    podstawy stringow

--- src/App/Exercises/Js/JsStringCoding/JsStringCoding.jsx
+++ src/App/Exercises/Js/JsStringCoding/JsStringCoding.jsx
@@ -17,6 +17,17 @@ const concatenated = string1 + ' ' + string3;
 
 {} ${} /*Obiekt lub fragment kodu*/
 
+// Metody stringów 
+
+const stringLength = singleQuote.length //5 bo ma 5 liter
+const substring = doubleQuote.substring(0, 2); // "si" od siema
+const replaceAllString = string1.replaceAll('o', '0') // hell0 w0rld bo wszystkie sie zmienia
+const replaceString = string1.replace('o', '0') // hell0 world bo tylko pierwsze sie zmieni
+const upperCase = string3.toUpperCase() // bedie z duzych liter IT'S RAINING OUTSIDE
+const trimString = templateString.trim() //
+const indexOfString = string2.indexOf('hel') // sprawdzic pozycje i szukanie konkretniej frazy ile razy bylo uzyte
+
+
 
   return <div>Kodowanie znakow </div>;
 };
